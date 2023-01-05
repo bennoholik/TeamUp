6f77617ebcad524a0ca7fb63ab59970baf3a8168
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2023-01-05 22:59:14 +0900

    [design] - 페이지 별 레이아웃 구성중

---
+++ components/Project/Create/PostingForm.tsx
@@ -0,0 +1,13 @@
+const Postingform = () => {
+  return (
+    <div className="max-w-[700px] w-full mx-auto py-8 px-6 bg-yellow-300">
+      <div>
+        <h1 className="text-2xl font-bold">프로젝트의 정보를 입력해주세요.</h1>
+
+        <div className="">c</div>
+      </div>
+    </div>
+  );
+};
+
+export default Postingform;
