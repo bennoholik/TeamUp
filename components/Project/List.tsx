292d469f8fc09b53b5f3f6698ff5ffdc4bb0340b
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2023-01-08 01:25:41 +0900

    [feat] - ProjectList 받아오기.

--- components/Project/List.tsx
+++ components/Project/List.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
 import PostCard from "../Card";
 
-const List = () => {
+const List = ({ projectList }) => {
+  console.log(projectList);
   const data = [
     1, 1, 1, 1, 1, 2, 3, 12, 31, 231, 1, 2, 2, 1234, 5, 1, 3, 5, 1, 2,
   ];
   return (
     <section className="2xl:max-w-[1280px] justify-center items-center bg-green-300 mx-auto flex flex-wrap gap-6 p-2">
-      {data.map((d, i) => (
-        <PostCard key={i} />
+      {projectList.map((pl, i) => (
+        <Link href={`project/${pl.questId}`} key={i}>
+          <PostCard key={pl.questId} />
+        </Link>
       ))}
     </section>
   );
