35009ffd6b755ad8451b4799ad406597e50e2bad
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2023-01-10 01:07:09 +0900

    [Design-fix] - 상세페이지 디자인 수정중

--- components/Project/Detail/Detail.tsx
+++ components/Project/Detail/Detail.tsx
@@ -12,10 +12,13 @@ const ProjectDetail = () => {
   );
 
   return (
-    <div className="max-w-[700px] w-full mx-auto py-8 px-6">
-      <h1 className="font-bold text-2xl"> {projectDetail?.title}</h1>
-      <div className="flex justify-between  mt-6">
-        <div className="flex gap-x-3">
+    <div className="max-w-[700px] min-h-screen w-full mx-auto py-8 px-6">
+      <h1 className="font-bold text-[35px] max_sm:text-[20px]">
+        {" "}
+        {projectDetail?.title}
+      </h1>
+      <div className="flex justify-between content-center mt-6">
+        <div className="flex gap-x-3 h-[40px]">
           <Image
             src={projectDetail ? projectDetail.profileImg : ""}
             alt="profileImg"
@@ -23,67 +26,75 @@ const ProjectDetail = () => {
             width={40}
             height={40}
           />
-          <span>{projectDetail?.nickname}</span>
+          <span className="mt-1 font-bold text-[20px] ">
+            {projectDetail?.nickname}
+          </span>
         </div>
-        <span>2023-01-04</span>
+        <span className="text-gray-400 mt-1.5">2023-01-04</span>
+      </div>
+      <hr className="mt-6 border" />
+
+      <div className="mt-20">
+        <h2 className="text-2xl font-bold text-gray-800">프로젝트 일정</h2>
+        <div className="grid grid-cols-2 mt-6">
+          <p>
+            <span className="font-bold text-gray-800">작업기간 :</span>{" "}
+            {projectDetail?.duration}주
+          </p>
+          <p>
+            <span className="font-bold text-gray-800">시작예정 :</span>{" "}
+            2023.01.04
+          </p>
+        </div>
       </div>
 
-      <div className="mt-10 flex flex-col gap-y-8">
-        <div className="grid grid-cols-2">
-          <span>작업기간 : {projectDetail?.duration}주</span>
-          <span>시작예정 : 2023.01.04</span>
-        </div>
-        <div className="flex">
-          <span className="mr-4 w-[200px]">기술 스택</span>
-          <ul className="flex gap-1 flex-wrap">
-            {projectDetail?.stacks.map((stack, idx) => (
-              <li
-                className="bg-gray-700 inline-block text-white whitespace-nowrap px-3 py-1.5 rounded-3xl"
-                key={idx}
-              >
-                {stack}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="mt-20">
+        <span className="text-2xl font-bold text-gray-800">기술 스택</span>
+        <ul className="flex mt-6 gap-1 flex-wrap">
+          {projectDetail?.stacks.map((stack, idx) => (
+            <li
+              className="bg-gray-600 inline-block text-white whitespace-nowrap px-3 py-1.5 rounded-3xl"
+              key={idx}
+            >
+              {stack}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="mt-10">
-        <h3>모집 현황</h3>
+
+      <div className="mt-20">
+        <h3 className="font-bold text-gray-800 text-2xl">모집 현황</h3>
         <ul className="grid gap-2 w-full grid-cols-2 mt-6">
-          <li className="grid gap-2 grid-cols-2">
-            <p>프론트엔드</p>
+          <li className="grid gap-2 grid-cols-3">
+            <p>프론트엔드 </p>
             <p>
-              :
               <span className="font-bold">
                 {projectDetail?.classes.frontend}
               </span>
               명
             </p>
           </li>
-          <li className="grid gap-2 grid-cols-2">
+          <li className="grid gap-2 grid-cols-3">
             <p>백엔드 </p>
             <p>
-              :
               <span className="font-bold">
                 {projectDetail?.classes.backend}
               </span>
               명
             </p>
           </li>
-          <li className="grid gap-2 grid-cols-2">
+          <li className="grid gap-2 grid-cols-3">
             <p>디자이너 </p>
             <p>
-              :
               <span className="font-bold">
                 {projectDetail?.classes.designer}
               </span>
               명
             </p>
           </li>
-          <li className="grid gap-2 grid-cols-2">
+          <li className="grid gap-2 grid-cols-3">
             <p>풀스택 </p>
             <p>
-              :
               <span className="font-bold">
                 {projectDetail?.classes.fullstack}
               </span>
@@ -92,14 +103,14 @@ const ProjectDetail = () => {
           </li>
         </ul>
 
-        <div className=" w-full mt-10">
-          <h2>소개글</h2>
+        <div className=" w-full mt-20">
+          <h2 className="font-bold text-gray-800 text-2xl mb-8">소개글</h2>
           <span className="whitespace-pre-line break-all">
             {projectDetail?.content}
           </span>
         </div>
 
-        <button className=" bg-gray-300 p-4">참가신청</button>
+        {/* <button className=" bg-gray-300 p-4">참가신청</button> */}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: { min: "480px" },
+        md: { min: "640px" },
+        lg: { min: "960px" },
+        xl: { min: "1366px" },
+        max_sm: { max: "480px" },
+        max_md: { max: "640px" },
+        max_lg: { max: "960px" },
+        max_xl: { max: "1366px" },
+      },
+    },
   },
   plugins: [],
 };
