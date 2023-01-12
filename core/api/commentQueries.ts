cd7d0a00570bd6ffde800b56de3fe5816ded1651
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2023-01-12 21:30:30 +0900

    [feat] - 댓글 부분 구구현중

--- core/api/commentQueries.ts
+++ core/api/commentQueries.ts
@@ -1,11 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { IProject } from "../types/projectType";
 import { commentApi } from "./apis";
 
 
 export const commentQueries = {
  useGetCommentList: (projectId: string) => {
-  return useQuery<IProject>(["comment",projectId], ()=>{
+  return useQuery(["comment",projectId], ()=>{
     return commentApi.getCommentList(projectId)
   })
  }
